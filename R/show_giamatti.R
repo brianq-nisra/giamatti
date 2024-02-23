5574725c7629e6ccc04eb143e69765f3545ae8c0
brianq-nisra <brian.quinn@nisra.gov.uk>
2024-02-23 14:46:09 +0000

    print message

--- R/show_giamatti.R
+++ R/show_giamatti.R
@@ -5,6 +5,8 @@ show_giamatti <- function() {
 
   #' @importFrom magick image_read
 
-  magick::image_read(system.file("img/giamatti.jpg", package = "giamatti"))
+  print(magick::image_read(system.file("img/giamatti.jpg", package = "giamatti")), info = FALSE)
+
+  print("You are looking at a picture of Paul Giamatti.")
 
 }
